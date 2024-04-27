248407100adcada6eda4d748eac67c64df9f89ac
Abu Huraira <abuhuraira.dt@gmail.com>
2024-04-28 02:27:25 +0300

    feat: add nav bar

---
+++ frontend/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+
+    <NuxtPage :page-key="route => route.fullPath" />
+
+  </div>
+</template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
   theme: {
     extend: {},
   },
